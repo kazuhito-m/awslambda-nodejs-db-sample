e14e48c0f75263cffb02bcc107744cd3278d2180
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-07-24 16:23:56 +0900

    #2 マイグレーションしなおし。sequelize-cliにガッツリ依存して作成させるように。

---
+++ migrations/20160723071851-create-aws-product.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: function(queryInterface, Sequelize) {
+    return queryInterface.createTable('AwsProducts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      productName: {
+        type: Sequelize.STRING
+      },
+      releaseDate: {
+        type: Sequelize.DATE
+      },
+      miuraUse: {
+        type: Sequelize.BOOLEAN
+      },
+      defficultLevel: {
+        type: Sequelize.NUMBER
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: function(queryInterface, Sequelize) {
+    return queryInterface.dropTable('AwsProducts');
+  }
+};
---
+++ models/awsproduct.js
@@ -0,0 +1,17 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var AwsProduct = sequelize.define('AwsProduct', {
+    productName: DataTypes.STRING,
+    releaseDate: DataTypes.DATE,
+    miuraUse: DataTypes.BOOLEAN,
+    defficultLevel: DataTypes.NUMBER,
+    description: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return AwsProduct;
+};
