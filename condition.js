90076811ad0e619e68898b65ac545c71b026aa46
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-07-24 23:17:54 +0900

    #2 実装。基本的な送信値から検索条件を作ってDBを検索、という機能を一通り作成終えた。

--- condition.js
+++ condition.js
@@ -3,59 +3,63 @@
  * 検索条件VOクラス。
  */
 function Condition() {
+
 	const values = {};	// 内部プロパティ変数を貯める場所。
+
 	Object.defineProperty(this, 'productName', makeAccesserProductName(values));
 	Object.defineProperty(this, 'miuraUse', makeAccesserMiuraUse(values));
+
+	// FIXME プロパティを実現するのに…こんなに複雑なことしなくても本当は行けるはず…。
+
+	function makeAccesserProductName(values) {
+		return {
+			enumerable: true,
+			set: function (value) {
+				values.productName = value;
+			},
+			get: function () {
+				return values.productName;
+			}
+		};
+	}
+
+	function makeAccesserMiuraUse(values) {
+		return {
+			enumerable: true,
+			set: function (value) {
+				if (value == null || typeof value === 'boolean') {
+					values.miuraUse = value;
+				} else {
+					throw new TypeError('101');
+				}
+			},
+			get: function () {
+				return values.miuraUse;
+			}
+		};
+	}
+
 }
 
 /**
  * Conditionのインスタンスに、API Gateway & Lambda で飛んできた「eventオブジェクt」の値をセットする。
  * 出来ない場合、0以外の数値を返す。
  */
-Condition.prototype.setAndValidation = (event) => {
+Condition.prototype.setAndValidation = (event, cond) => {
 
 	const productName = nullize(event.productName);
 	if (!isTypeNullable('string', productName)) {
 		return 101;
 	}
-	this.productName = productName;
+	cond.productName = productName;
 
 	const miuraUse = nullize(event.miuraUse);
-	if (!isTypeNullable('boolean', productName)) {
+	if (!isTypeNullable('boolean', miuraUse)) {
 		return 102;
 	}
-	this.miuraUse = miuraUse;
+	cond.miuraUse = miuraUse;
 
 	return 0;
-}
-
-function makeAccesserProductName(values) {
-	return {
-		enumerable: true,
-		set: function (value) {
-			values.productName = value;
-		},
-		get: function () {
-			return values.productName;
-		}
-	};
-}
-
-function makeAccesserMiuraUse(values) {
-	return {
-		enumerable: true,
-		set: function (value) {
-			if (value == null || typeof value === 'boolean') {
-				values.miuraUse = value;
-			} else {
-				throw new TypeError('101');
-			}
-		},
-		get: function () {
-			return values.miuraUse;
-		}
-	};
-
 }
 
 /**
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ exports.handler = (event, context) => {
   // API Gateway & Lambda に来た「JSONパラメータ」を
   // バリデーションかつ検索用オブジェクト化。
   const condition = new Condition();
-  const errCode = condition.setAndValidation(event);
+  const errCode = condition.setAndValidation(event, condition); // FIXME 自分を引数にせなあかんとか…腐ってる
   if (errCode !== 0) {
     context.succeed({ result: errCode });
     return;
@@ -15,8 +15,23 @@ exports.handler = (event, context) => {
 
   const models = require('./models/index');
 
-  models.AwsProduct.findAll({}).then((records) => {
-    context.succeed(JSON.stringify(records));
+  // ORM用の「WHERE条件オブジェクト」作り。
+  const wheres = {};
+  let condCount = 0;
+  if (condition.productName !== null) {
+    wheres.productName = { $iLike: '%' + condition.productName + '%' };
+  }
+  if (condition.miuraUse !== null) {
+    wheres.miuraUse = condition.miuraUse;
+  }
+  const findSettings = {};
+  if (Object.keys(wheres).length > 0) {
+    findSettings.where = wheres;
+  }
+
+  // 検索を実行し、結果のオブジェクトをJSON化してレスポンスとして返す。
+  models.AwsProduct.findAll(findSettings).then((records) => {
+    context.succeed({ result: 0, data: records });
   });
 
 };
