0b46b7218855a6de0b44f5de82ccf9038d280646
kazuhito-m <kazuhito.sumpic@gmail.com>
2016-07-30 22:34:27 +0900

    テスト拡充。DBテスト以外の箇所にはカバーするようにテストを追加。

---
+++ src/test/index_test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const assert = require('power-assert');
+
+/**
+ * AWS Lambda の本体関数のテスト。
+ */
+
+describe('lambda.handler', () => {
+
+    // TODO 「DBテスト」の方法がわからない限り実装しない
+
+    it('Lambdaに届いた時に入力が変なら"code"に0以外が返る', function() {
+        // 事前条件
+        const event = {
+            productName: 0,
+            miuraUsr: 0
+        };
+
+        // 実行 & 検証
+        const context = {
+            succeed: (result) => {
+                // コールバックが終点なので、そこで検証する。
+                assert.notEqual(result.code, 0, 'エラー時はcodeが0以外');
+                assert.equal(result.data, undefined, 'エラー時はデータが空っぽ');
+            }
+        };
+
+        const lambda =ｚ require('../main/index');
+        lambda.handler(event, context);
+    });
+
+});
---
+++ src/test/xxxdao_test.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const assert = require('power-assert');
+
+const XxxDao = require('../main/xxxdao');
+
+/**
+ * XxxDaoクラスのテスト。
+ */
+
+describe('XxxDao', () => {
+
+    // TODO 未実装
+    // TODO 「DBテスト」の方法がわからない限り実装しない
+
+});
